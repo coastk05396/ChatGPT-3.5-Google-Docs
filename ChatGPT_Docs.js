894d393e5ae0f33121d7ebe1a6209f5307e98709
Rick <coastk05396@gmail.com>
2023-03-13 16:44:53 +0800

    update demo

--- ChatGPT_Docs.js
+++ ChatGPT_Docs.js
@@ -9,7 +9,7 @@ function onOpen() {
       .addItem("產生貼文", "generateIdeas")
       .addItem("翻譯成英文", "TranslateToEn")
       .addItem("翻譯成中文", "TranslateToCh")
-      // .addItem("回覆Email", "ReplyEmail")
+      .addItem("回復Email", "ReplyEmail")
       // .addItem("翻譯英文", "generateIdeas")
       .addItem("詢問機器人", "menuItem")      
       .addToUi();
@@ -90,7 +90,7 @@ function TranslateToEn() {
   const doc = DocumentApp.getActiveDocument();
   const selectedText = doc.getSelection().getRangeElements()[0].getElement().asText().getText(); 
   const body = doc.getBody();
-  const prompt = "Help me translate" + selectedText + "in English";
+  const prompt = "Generate an English on" + selectedText;
   const temperature = 0.8;
   const maxTokens = 2060;
 
@@ -125,7 +125,44 @@ function TranslateToCh() {
   const doc = DocumentApp.getActiveDocument();
   const selectedText = doc.getSelection().getRangeElements()[0].getElement().asText().getText();
   const body = doc.getBody();
-  const prompt = "Help me translate" + selectedText + "in 台灣繁體中文";
+  const prompt = "翻譯成繁體中文" + selectedText;
+  const temperature = 0.8;
+  const maxTokens = 2060;
+
+  const requestBody = {
+    model: MODEL_TYPE,
+    messages: [{role: "user", content: prompt}],
+    temperature,
+    max_tokens: maxTokens,
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + API_KEY,
+    },
+    payload: JSON.stringify(requestBody),
+  };
+
+
+  const response = UrlFetchApp.fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+  const responseText = response.getContentText();
+  const json = JSON.parse(responseText);
+  const generatedText = json['choices'][0]['message']['content'];
+  Logger.log(generatedText);
+  body.appendParagraph(generatedText.toString());
+}
+
+
+
+
+// Reply Email
+function ReplyEmail() {
+  const doc = DocumentApp.getActiveDocument();
+  const selectedText = doc.getSelection().getRangeElements()[0].getElement().asText().getText();
+  const body = doc.getBody();
+  const prompt = "reply an email on" + selectedText;
   const temperature = 0.8;
   const maxTokens = 2060;
 
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
-##test
+### 1. 先填入OpenAI Key
+
+const API_KEY = "";
+
+![Demo0](/Demo/demo0.png)
+
+
+
+### 2. 開啟Google DOcs 並點及App Scripts
+
+![Demo1](/Demo/demo1.png)
+
+### 3. 複製code 貼上 並貼上 並點及save & run button
+
+![Demo2](/Demo/demo2.png)
+
+
+### 4. 去Docs 主頁 選取文字 並點選ChatGPT 功能
+
+![Demo3](/Demo/demo3.png)
